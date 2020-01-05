e934fcb210ed825b64b7fa088ea444e39814f697
Jorge Herrero <jorge.herrero.garcia.next@bbva.com>
2020-01-05 18:20:17 +0100

    commit example app react

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            App React Js <code>Ejemplo</code> - Jorge Herrero.
           </p>
           <a
             className="App-link"
